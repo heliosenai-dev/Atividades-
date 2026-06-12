8fd626d233d196c1f6ed7ffdf157b61e9bdb966d
Hélio Marcos (PC PESSOAL) <h.paixao@aluno.senai.br>
2026-06-12 09:59:35 -0300

    Corrijados as Atividades 1,2,3

---
+++ Pagina_03/Scripts_Questão03.js
@@ -0,0 +1,29 @@
+//PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#formulario')
+const divResultado = document.querySelector('#resultado')
+
+//CAPTURANDO O EVENTO DE SUBMIT
+formDados.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+
+    const forme_num = new FormData(formDados)
+
+let Distancekm = parseFloat(forme_num.get('Distancekm'));
+let consumed = parseFloat(forme_num.get('consumed'));
+ let prelitro = parseFloat(forme_num.get('prelitro'));
+   
+let quantcombus = Distancekm / consumed;
+let valorTotal = quantcombus * prelitro;
+
+let mensagem = "--- RESULTADOS DA VIAGEM ---\n";
+divResultado.innerHTML = "Quantidade de combustível necessária: " + quantcombus.toFixed(2) + " litros\n";
+ divResultado.innerHTML = "Valor total a pagar: R$ " + valorTotal.toFixed(2);
+     
+
+
+
+
+      
+
+
+})
